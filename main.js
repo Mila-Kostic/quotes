a344e3d2c4cbf70692925665c3b572ff379f3bb2
Mila-Kostic <milakostic.eng@gmail.com>
2022-05-24 11:55:20 +0200

    data

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 //alert('Happy codding!');
+
+var quotes = [
+    ' We become what we think about – Earl Nightingale',
+
+'A reminder that positive thinking helps you frame your mindset and change your life, spoken by the author of, “Essence of Success.”',
+
+'“People who are crazy enough to think they can change the world, are the ones who do.”- Rob Siltanen',
+
+' Rob is a leading creative marketer responsible for some of the most effective and iconic advertising campaigns of the past 30 years.',
+
+' “Optimism is the one quality more associated with success and happiness than any other.”- Brian Tracy',
+
+' Let your positivity and optimism inspire you and lead you to success. That’s my favorite way to achieve success.',
+
+' “Happiness is not something readymade. It comes from your own actions.”-Dalai Lama',
+
+' A visionary among us, the Dalai Lama always has something inspiring to share.',
+
+' “All our dreams can come true if we have the courage to pursue them.”- Walt Disney',
+
+' This amazing quote comes from the man who created the happiest place on earth – and a multibillion-dollar empire.',
+
+' “Success is not final, failure is not fatal: it is the courage to continue that counts.” Winston Churchill',
+
+' When it comes to inspirational, encouraging quotes by successful people, Winston Churchill’s words of wisdom always make my list.',
+
+' “Believe you can and you’re halfway there.” Theodore Roosevelt'
+];
+console.log(quotes);
